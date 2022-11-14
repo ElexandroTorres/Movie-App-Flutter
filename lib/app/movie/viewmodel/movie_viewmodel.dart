52f14c4a6bff2e91b6db25939105748710f1d744
Elexandro Torres <elexandro.torres@gmail.com>
2022-11-14 19:23:52 -0300

    changes in movie viewmodel to pass repository by argument

--- lib/app/movie/viewmodel/movie_viewmodel.dart
+++ lib/app/movie/viewmodel/movie_viewmodel.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/foundation.dart';
-import 'package:http/http.dart' as http;
 import 'package:movie_app_flutter/app/movie/model/movie.dart';
 import 'package:movie_app_flutter/app/movie/model/top_rated.dart';
 import 'package:movie_app_flutter/app/movie/repository/movie_repository.dart';
@@ -7,8 +6,9 @@ import 'package:movie_app_flutter/app/movie/repository/movie_repository.dart';
 enum LoadingStatus { empty, initialLoading, loading, success, error }
 
 class MovieViewModel extends ChangeNotifier {
-  final MovieRepository movieRepository =
-      MovieRepository(client: http.Client());
+  MovieViewModel({required this.movieRepository});
+
+  final MovieRepository movieRepository;
 
   List<Movie> _topRatedList = [];
   List<Movie> get topRatedList => _topRatedList;
