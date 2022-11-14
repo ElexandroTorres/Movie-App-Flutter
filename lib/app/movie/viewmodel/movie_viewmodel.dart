261877a9390b171078d7985a3de56ea3019bf9b5
Elexandro Torres <elexandro.torres@gmail.com>
2022-11-14 19:19:23 -0300

    movie viewmodel created

---
+++ lib/app/movie/viewmodel/movie_viewmodel.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'package:movie_app_flutter/app/movie/model/movie.dart';
+import 'package:movie_app_flutter/app/movie/model/top_rated.dart';
+import 'package:movie_app_flutter/app/movie/repository/movie_repository.dart';
+
+enum LoadingStatus { empty, initialLoading, loading, success, error }
+
+class MovieViewModel extends ChangeNotifier {
+  final MovieRepository movieRepository =
+      MovieRepository(client: http.Client());
+
+  List<Movie> _topRatedList = [];
+  List<Movie> get topRatedList => _topRatedList;
+
+  LoadingStatus _loadingStatus = LoadingStatus.empty;
+  LoadingStatus get loadingStatus => _loadingStatus;
+  TopRated? _topRated;
+  int _currentPage = 1;
+
+  Future<void> getTopRated() async {
+    try {
+      if (_currentPage == 1) {
+        _loadingStatus = LoadingStatus.loading;
+      }
+
+      notifyListeners();
+      _topRated = await movieRepository.getTopRatedMovies(_currentPage);
+      _topRatedList.addAll(_topRated!.results!);
+
+      if (_topRated != null) {
+        _loadingStatus = LoadingStatus.success;
+        _currentPage++;
+        notifyListeners();
+      }
+    } catch (error) {
+      _loadingStatus = LoadingStatus.error;
+      notifyListeners();
+    }
+  }
+
+  void resetConfigs() {
+    _currentPage = 1;
+    _topRatedList = [];
+    _loadingStatus = LoadingStatus.empty;
+    notifyListeners();
+  }
+}
